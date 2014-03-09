90240a60a1b58d464c80bffc159ab588890d5da4
nelson <nelson@nelson-PC>
2014-03-09 19:52:07 +0100

    preparación de la página web

--- backadmin/backIndex.php
+++ backadmin/backIndex.php
@@ -1,2 +1,22 @@
 <?php
+session_start();
+if(isset($_SESSION['usuario']))
+    {
+        echo 'est&aacute;s en el backend '.$_SESSION['usuario'];
+       // session_destroy();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    <body>
+        
+    </body>
+</html>
+<?php
+    }else
+        {
+        echo 'acceso no permitido';
+        }
 ?>

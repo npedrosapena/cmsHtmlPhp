9aba0fc2e41260802ee0a55b16a3b181dfe07408
nelson <nelson@nelson-PC>
2014-03-09 21:31:43 +0100

    añadido primer botón de configuración con el css

--- backadmin/backIndex.php
+++ backadmin/backIndex.php
@@ -2,16 +2,32 @@
 session_start();
 if(isset($_SESSION['usuario']))
     {
-        echo 'est&aacute;s en el backend '.$_SESSION['usuario'];
-       // session_destroy();
 ?>
 <!DOCTYPE html>
 <html>
     <head>
-        
+        <link type="text/css" rel="stylesheet" href="../css/admin.css" />
+        <title>Administraci&oacute;n</title>
     </head>
     <body>
-        
+        <div id="contentWrapper">
+            <div id="wrapper">
+                <div id="superior">
+                    
+                </div>
+                <div id="cuerpo">
+                    <ul class="categorias">
+                        <li class="ElementosCategorias" id="config">
+                            <a href="#" class="content">
+                                <img src="../icos/appbar.cog.png" alt="Configuraci&oacute;n"/>
+                                <h3>Configuraci&oacute;n</h3>
+                                <p class="sumary"> Acceder a la configuraci&oacute;n</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
 <?php

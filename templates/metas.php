7f797b8659194ee0035c8f327c4b3af9d06ac4ab
nelson <nelson@nelson-PC>
2014-03-09 19:51:25 +0100

    añadida plantilla con etiquetas meta

---
+++ templates/metas.php
@@ -0,0 +1,5 @@
+
+<meta name="description" content="variables en php ">
+<meta name="keywords" content="lista palabrabas clave" >
+<meta name="author" content="variables creador de la web" >
+<meta charset="UTF-8" >

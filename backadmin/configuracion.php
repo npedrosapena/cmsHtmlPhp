d24c7871fb89b8673239e54e584f195d42b07e17
Nelson Pedrosa <mazaroca_de_millo@hotmail.com>
2014-03-31 16:53:01 +0200

    pagina configuracion

---
+++ backadmin/configuracion.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include_once '../templates/metas.php'; ?>
+        <link type="text/css" rel="stylesheet" href="../css/configuracion.css" />
+        <title>Configuraci&oacute;n</title>
+    </head>
+    <body>
+        <div>
+            <h3>Configuraci&oacute;n</h3>
+        </div>
+        <div id="formularioConfiguracion">
+            <fieldset>
+                <ul>
+                    <li><a href="#">Metaetiquetas</a></li>
+                    <li><a href="#">Usuarios</a></li>
+                    <li><a href="#">T&iacute;tulo de la web</a></li>
+                </ul>
+            </fieldset>
+        </div>
+    </body>
+    
+</html>

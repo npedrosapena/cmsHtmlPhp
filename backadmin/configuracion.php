78e6553f2c9819cf8f1351a60542e11efcd2876a
Nelson Pedrosa <mazaroca_de_millo@hotmail.com>
2014-03-31 17:26:07 +0200

    añadidos algunos elementos del menú de configuración

--- backadmin/configuracion.php
+++ backadmin/configuracion.php
@@ -6,15 +6,30 @@
         <title>Configuraci&oacute;n</title>
     </head>
     <body>
-        <div>
+        <div id="titulo">
             <h3>Configuraci&oacute;n</h3>
         </div>
         <div id="formularioConfiguracion">
             <fieldset>
-                <ul>
-                    <li><a href="#">Metaetiquetas</a></li>
-                    <li><a href="#">Usuarios</a></li>
-                    <li><a href="#">T&iacute;tulo de la web</a></li>
+                <ul class="categorias">
+                    <li class="ElementosCategorias" id="config">
+                        <a href="#" class="content">
+                            <img src="../icos/appbar.cog.png" alt="Configuraci&oacute;n"/>
+                            <h3>Metaetiquetas</h3>
+                            <p class="sumary"> Acceder a las Metaetiquetas</p>
+                        </a></li>
+                    <li class="ElementosCategorias" id="config">
+                        <a href="#" class="content">
+                             <img src="../icos/appbar.cog.png" alt="Configuraci&oacute;n"/>
+                             <h3>Usuarios</h3>
+                             <p class="sumary"> Acceder la configuraci&oacute;n de los usuarios</p>
+                        </a></li>
+                    <li class="ElementosCategorias" id="config">
+                        <a href="#" class="content">
+                             <img src="../icos/appbar.cog.png" alt="Configuraci&oacute;n"/>
+                             <h3>T&iacute;tulo de la web</h3>
+                             <p class="sumary"> Acceder al t&iacute;tulo de la web</p>
+                        </a></li>
                 </ul>
             </fieldset>
         </div>

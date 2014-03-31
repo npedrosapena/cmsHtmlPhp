35e2d0892af88c5190228c2f7092264bfda2e9c8
Nelson Pedrosa <mazaroca_de_millo@hotmail.com>
2014-03-31 16:54:25 +0200

    añadido otro include para gestionar el añadido de otros archivos. Más adelante tocará corregirlo

--- templates/metas.php
+++ templates/metas.php
@@ -1,14 +1,14 @@
-<?php include_once 'objs/bbdd.php';
+<?php 
+    @include 'objs/bbdd.php';
+    @include '../objs/bbdd.php';
 
 $BD=new Conexion();
-$meta1=$BD->categoriasMeta(1);
-$meta2=$BD->consultas(1);
 $contador=$BD->contadorRows("categoriametas");
 
 for($c=1;$c<=($contador[0]);$c++):
     $name=$BD->categoriasMeta($c);
     $content=$BD->descripcionMetas($name["name"]);
-    echo '<meta name="'.$name["name"].'" aaaaaacontent="'.$content["content"].'"/>';
+    echo '<meta name="'.$name["name"].'" content="'.$content["content"].'"/>';
 endfor;
 
 echo '<meta charset="UTF-8" >';

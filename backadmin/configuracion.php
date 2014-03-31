a591de17e4f11ef18a16e9b1ce07b96f20232f0a
Nelson Pedrosa <mazaroca_de_millo@hotmail.com>
2014-03-31 17:44:39 +0200

    añadidos iconos a las opciones

--- backadmin/configuracion.php
+++ backadmin/configuracion.php
@@ -14,19 +14,19 @@
                 <ul class="categorias">
                     <li class="ElementosCategorias" id="config">
                         <a href="#" class="content">
-                            <img src="../icos/appbar.cog.png" alt="Configuraci&oacute;n"/>
+                            <img src="../icos/appbar.list.check.png" alt="Configuraci&oacute;n"/>
                             <h3>Metaetiquetas</h3>
                             <p class="sumary"> Acceder a las Metaetiquetas</p>
                         </a></li>
                     <li class="ElementosCategorias" id="config">
                         <a href="#" class="content">
-                             <img src="../icos/appbar.cog.png" alt="Configuraci&oacute;n"/>
+                            <img src="../icos/appbar.group.png" alt="Configuraci&oacute;n"/>
                              <h3>Usuarios</h3>
                              <p class="sumary"> Acceder la configuraci&oacute;n de los usuarios</p>
                         </a></li>
                     <li class="ElementosCategorias" id="config">
                         <a href="#" class="content">
-                             <img src="../icos/appbar.cog.png" alt="Configuraci&oacute;n"/>
+                            <img src="../icos/appbar.list.add.above.png" alt="Configuraci&oacute;n"/>
                              <h3>T&iacute;tulo de la web</h3>
                              <p class="sumary"> Acceder al t&iacute;tulo de la web</p>
                         </a></li>

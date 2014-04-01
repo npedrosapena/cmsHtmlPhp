4e92663f692ee40fe3d56cc2a94b2b9a6b1dd00c
Nelson Pedrosa <mazaroca_de_millo@hotmail.com>
2014-04-01 10:28:54 +0200

    añadido enlace javascript

--- backadmin/configuracion.php
+++ backadmin/configuracion.php
@@ -3,6 +3,7 @@
     <head>
         <?php include_once '../templates/metas.php'; ?>
         <link type="text/css" rel="stylesheet" href="../css/configuracion.css" />
+         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script> 
         <title>Configuraci&oacute;n</title>
     </head>
     <body>
@@ -12,19 +13,19 @@
         <div id="formularioConfiguracion">
             <fieldset>
                 <ul class="categorias">
-                    <li class="ElementosCategorias" id="config">
-                        <a href="#" class="content">
+                    <li class="ElementosCategorias" id="meta">
+                        <a href="javascript:lector()" class="content">
                             <img src="../icos/appbar.list.check.png" alt="Configuraci&oacute;n"/>
                             <h3>Metaetiquetas</h3>
                             <p class="sumary"> Acceder a las Metaetiquetas</p>
                         </a></li>
-                    <li class="ElementosCategorias" id="config">
+                    <li class="ElementosCategorias" id="users">
                         <a href="#" class="content">
                             <img src="../icos/appbar.group.png" alt="Configuraci&oacute;n"/>
                              <h3>Usuarios</h3>
                              <p class="sumary"> Acceder la configuraci&oacute;n de los usuarios</p>
                         </a></li>
-                    <li class="ElementosCategorias" id="config">
+                    <li class="ElementosCategorias" id="title">
                         <a href="#" class="content">
                             <img src="../icos/appbar.list.add.above.png" alt="Configuraci&oacute;n"/>
                              <h3>T&iacute;tulo de la web</h3>
@@ -33,6 +34,7 @@
                 </ul>
             </fieldset>
         </div>
+        <script src="../js/configuracion.js"></script> 
     </body>
     
 </html>

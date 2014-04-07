a90bec6e5a5843c880efdc0892b6155a8aa27ea6
Nelson Pedrosa <mazaroca_de_millo@hotmail.com>
2014-04-07 17:42:17 +0200

    añadidos divs para formularios de datos

--- backadmin/configuracion.php
+++ backadmin/configuracion.php
@@ -11,28 +11,37 @@
             <h3>Configuraci&oacute;n</h3>
         </div>
         <div id="formularioConfiguracion">
-            <fieldset>
+            <div id="menuConfig">
                 <ul class="categorias">
                     <li class="ElementosCategorias" id="meta">
-                        <a href="javascript:lector()" class="content">
+                        <a href="javascript:lector('meta')" class="content">
                             <img src="../icos/appbar.list.check.png" alt="Configuraci&oacute;n"/>
                             <h3>Metaetiquetas</h3>
                             <p class="sumary"> Acceder a las Metaetiquetas</p>
                         </a></li>
                     <li class="ElementosCategorias" id="users">
-                        <a href="#" class="content">
+                        <a href="javascript:lector('users')" class="content">
                             <img src="../icos/appbar.group.png" alt="Configuraci&oacute;n"/>
                              <h3>Usuarios</h3>
                              <p class="sumary"> Acceder la configuraci&oacute;n de los usuarios</p>
                         </a></li>
                     <li class="ElementosCategorias" id="title">
-                        <a href="#" class="content">
+                        <a href="javascript:lector('title')" class="content">
                             <img src="../icos/appbar.list.add.above.png" alt="Configuraci&oacute;n"/>
                              <h3>T&iacute;tulo de la web</h3>
                              <p class="sumary"> Acceder al t&iacute;tulo de la web</p>
                         </a></li>
                 </ul>
-            </fieldset>
+            </div>
+        </div>
+        <div id="divmeta">
+            <h1>Metaetiquetas</h1>
+        </div>
+        <div id="divusers">
+            <h1>Usuarios</h1>
+        </div>
+        <div id="divtitle">
+            <h1>Titulo de la web</h1>
         </div>
         <script src="../js/configuracion.js"></script> 
     </body>

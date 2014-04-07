639a208d0b24dff1fda22b7df9f50bea16a3592d
Nelson Pedrosa <mazaroca_de_millo@hotmail.com>
2014-04-07 17:37:11 +0200

    añadido efecto in/out V1

--- js/configuracion.js
+++ js/configuracion.js
@@ -1,9 +1,85 @@
 
-
-function lector()
+$(document).ready(function()
 {
-alert("hola");
-var valor= document.getElementById("meta");
-alert(valor);
+    $('div#divmeta').hide();
+    $('div#divusers').hide();
+    $('div#divtitle').hide();
+   
+   $("#meta").click(function () {
+      $("#divmeta").each(function() {
+        displaying = $(this).css("display");
+        if(displaying === "block") {
+          $(this).fadeOut('slow',function() {
+           $(this).css("display","none");
+           $('#divtitle').fadeOut('slow');
+           $('#divusers').fadeOut('slow');
+          });
+        } else {
+          $(this).fadeIn('slow',function() {
+            $(this).css("display","block");
+            $('#divtitle').fadeOut('fast');
+            $('#divusers').fadeOut('fast');
+          });
+        }
+      });
+    });
+    
+    $("#users").click(function () {
+      $("#divusers").each(function() {
+        displaying = $(this).css("display");
+        if(displaying === "block") {
+          $(this).fadeOut('slow',function() {
+           $('#divtitle').fadeOut('slow');
+           $('#divmeta').fadeOut('slow');
+           $(this).css("display","none");
+          });
+        } else {
+          $(this).fadeIn('slow',function() {
+            //$('#divtitle').slidedown();
+            $('#divtitle').fadeOut('fast');
+            //$('#divmeta').slidedown();
+            $('#divmeta').fadeOut('slow');
+            $(this).css("display","block");
+          });
+        }
+      });
+    });
+    
+    $("#title").click(function () {
+      $("#divtitle").each(function() {
+        displaying = $(this).css("display");
+        if(displaying === "block") {
+          $(this).fadeOut('slow',function() {
+           //$('#divusers').slideup();
+           $('#divusers').fadeOut('fast');
+           //$('#divmeta').slideup();
+           $('#divmeta').fadeOut('slow');
+           $(this).css("display","none");
+          });
+        } else {
+          $(this).fadeIn('slow',function() {
+           // $('#divusers').slideup();
+            $('#divusers').fadeOut('fast');
+            //$('#divmeta').slideup();
+            $('#divmeta').fadeOut('fast');
+            $(this).css("display","block");
+          });
+        }
+      });
+    });
+    
+    
+    
+});
+
+
+
+function lector($Id)
+{
+    var campo="";
+    var res=campo.concat("'div#div",$Id,"'");
+   // alert(res.valueOf());
+
+    $(res).fadeIn(2000);
 }
 

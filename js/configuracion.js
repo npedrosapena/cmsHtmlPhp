d1c5c972c31c8823af44d5d853bb2cd2efb638b9
Nelson Pedrosa <mazaroca_de_millo@hotmail.com>
2014-05-12 16:56:00 +0200

    modificado efecto in/out

--- js/configuracion.js
+++ js/configuracion.js
@@ -5,75 +5,30 @@ $(document).ready(function()
     $('div#divusers').hide();
     $('div#divtitle').hide();
    
-   $("#meta").click(function () {
-      $("#divmeta").each(function() {
-        displaying = $(this).css("display");
-        if(displaying === "block") {
-          $(this).fadeOut('slow',function() {
-           $(this).css("display","none");
-           $('#divtitle').fadeOut('slow');
-           $('#divusers').fadeOut('slow');
-          });
-        } else {
-          $(this).fadeIn('slow',function() {
-            $(this).css("display","block");
-            $('#divtitle').fadeOut('fast');
-            $('#divusers').fadeOut('fast');
-          });
-        }
-      });
+   $("#meta").click(function () 
+   {
+       $("#divtitle").hide(600);
+       $("#divusers").hide(600);
+       $("#divmeta").show(800);
     });
     
-    $("#users").click(function () {
-      $("#divusers").each(function() {
-        displaying = $(this).css("display");
-        if(displaying === "block") {
-          $(this).fadeOut('slow',function() {
-           $('#divtitle').fadeOut('slow');
-           $('#divmeta').fadeOut('slow');
-           $(this).css("display","none");
-          });
-        } else {
-          $(this).fadeIn('slow',function() {
-            //$('#divtitle').slidedown();
-            $('#divtitle').fadeOut('fast');
-            //$('#divmeta').slidedown();
-            $('#divmeta').fadeOut('slow');
-            $(this).css("display","block");
-          });
-        }
-      });
+    $("#users").click(function () 
+    {
+       $("#divtitle").hide(600);
+       $("#divmeta").hide(600);
+       $("#divusers").show(800);
     });
     
-    $("#title").click(function () {
-      $("#divtitle").each(function() {
-        displaying = $(this).css("display");
-        if(displaying === "block") {
-          $(this).fadeOut('slow',function() {
-           //$('#divusers').slideup();
-           $('#divusers').fadeOut('fast');
-           //$('#divmeta').slideup();
-           $('#divmeta').fadeOut('slow');
-           $(this).css("display","none");
-          });
-        } else {
-          $(this).fadeIn('slow',function() {
-           // $('#divusers').slideup();
-            $('#divusers').fadeOut('fast');
-            //$('#divmeta').slideup();
-            $('#divmeta').fadeOut('fast');
-            $(this).css("display","block");
-          });
-        }
-      });
+    $("#title").click(function ()
+    {
+       $("#divusers").hide(600);
+       $("#divmeta").hide(600);
+       $("#divtitle").show(800);
     });
-    
-    
-    
 });
 
 
-
+/*
 function lector($Id)
 {
     var campo="";
@@ -82,4 +37,4 @@ function lector($Id)
 
     $(res).fadeIn(2000);
 }
-
+*/
